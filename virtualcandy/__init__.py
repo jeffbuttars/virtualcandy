9e267e41c4dd138937a09d938836878b80a6c87d
Jeff Buttars <jeffbuttars@gmail.com>
2017-04-21 11:21:59 -0600

    Add init

---
+++ virtualcandy/__init__.py
@@ -0,0 +1,8 @@
+# version is a human-readable version number.
+
+# http://legacy.python.org/dev/peps/pep-0440/#version-scheme
+# Use the pep-0440 as a versioning guidline
+# There are always four parts, although trailing parts 'may' be empty.
+# Idealy the first 3 parts will always have a value
+__version__ = "0.1.0.dev1"
+__version_info__ = ('0', '1', '0', 'dev1')
