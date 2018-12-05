40a545b5eb534caf9611091d5393f325856579b1
Jeff Buttars <jeffbuttars@gmail.com>
2018-12-04 19:20:07 -0700

    version 1.0.0

--- virtualcandy/__init__.py
+++ virtualcandy/__init__.py
@@ -4,5 +4,5 @@
 # Use the pep-0440 as a versioning guidline
 # There are always four parts, although trailing parts 'may' be empty.
 # Idealy the first 3 parts will always have a value
-__version_info__ = ('0', '9', '1')
+__version_info__ = ('1', '0', '0')
 __version__ = '.'.join(__version_info__)
