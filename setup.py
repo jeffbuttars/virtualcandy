49a5a7441b7debb2e6f7e67b37234dac7c22b9af
Jeff Buttars <jeffbuttars@gmail.com>
2018-12-04 19:16:05 -0700

    add vcshellinstall shell script to ease installation

--- setup.py
+++ setup.py
@@ -1,31 +1,12 @@
 #!/usr/bin/env python
 
-
 import os
 import sys
-from pip.req import parse_requirements
-from pip.download import PipSession
 from setuptools import setup
 
 THIS_DIR = os.path.basename(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(THIS_DIR, 'virtualcandy'))
 import virtualcandy
-
-
-def get_requires(rname='requirements.txt'):
-    this_dir = os.path.realpath(os.path.dirname(__file__))
-    fname = os.path.join(this_dir, rname)
-    res = []
-
-    # We work around a pip bug here.
-    try:
-        reqs = parse_requirements(fname)
-        res = [str(ir.req) for ir in reqs]
-    except TypeError:
-        reqs = parse_requirements(fname, session=PipSession())
-        res = [str(ir.req) for ir in reqs]
-
-    return res
 
 
 setup(
@@ -40,17 +21,15 @@ setup(
 
     long_description=open('README.rst').read(),
 
-    # classifiers=[
-    #     'License :: OSI Approved :: Apache Software License',
-    #     'Operating System :: POSIX',
-    #     'Programming Language :: Python',
-    #     'Programming Language :: Python :: 2.6',
-    #     'Programming Language :: Python :: 2.7',
-    #     'Topic :: Internet',
-    # ],
+    classifiers=[
+       'License :: OSI Approved :: Apache Software License',
+       'Operating System :: POSIX',
+       'Programming Language :: Python',
+       'Topic :: Internet',
+    ],
 
-    install_requires=get_requires() + [],
-    scripts=['virtualcandy/vcpkgs'],
+    install_requires=['pipenv'],
+    scripts=['virtualcandy/vcshellinstaller'],
     package_data={'virtualcandy': ['lib/*', 'lib/tmpl/*']},
 
     #  data_files=[
